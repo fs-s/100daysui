d37de5bf66bd7f90347e73b08c6bb1fcb67e8353
fs-s <joonas.puuding@ametikool.ee>
2021-01-11 14:04:40 +0200

    yesy

--- src/views/UI-002.vue
+++ src/views/UI-002.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="flex-center">
-    <div class="home-content">
-      <h1>Siia tuleb teine ülesanne (sign-up form)</h1>
-      <p>
-        Challenge: Design Hint... Create a sign up page, modal, form, or app
-        screen related to signing up for something. It could be for a volunteer
-        event, contest registration, a giveaway, or anything you can image.
-      </p>
+    <div class="login-card">
+
+        <h1>Do you need help?</h1>
+
     </div>
   </div>
 </template>
+
+
+
+
+
+<style lang="scss" scoped>
+.login {
+  &-card {
+    text-align: left;
+    background: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    padding: 10rem 6rem;
+    //background:blue;
+    background-image: url(https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png);
+    background-size: 500px 500px;
+    background-repeat: no-repeat;
+    h1 {
+      margin-top: 0;
+      color: white;
+      height: 100px;
+      display: flex;
+    }
+    h2{
+      display: flex;
+      padding: 10px;
+      text-align: center;
+      position:relative;
+}
+  }
+}
+</style>
